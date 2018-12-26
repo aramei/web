82069e8fd6eb465c8aa76b5b566d9d7a33a05e7f
aramei <pasichnyk89@gmail.com>
2018-12-26 22:38:40 +0200

    Added shell script file

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &
