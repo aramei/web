ffeb3010c5db2361e5b7f51393c9dd8683ca1836
aramei <pasichnyk89@gmail.com>
2018-12-30 22:36:07 +0200

    Added django app

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,20 +1,20 @@
 workers = 3              # number of workers Gunicorn will spawn
 
-bind = '127.0.0.1:8000'  # this is where you declare on which address your
+bind = '0.0.0.0:8080'  # this is where you declare on which address your
                          # gunicorn app is running.
                          # Basically where Nginx will forward the request to
 
-pidfile = '/var/run/gunicorn/mysite.pid' # create a simple pid file for gunicorn.
+#pidfile = '/var/run/gunicorn/mysite.pid' # create a simple pid file for gunicorn.
 
-user = 'user'          # the user gunicorn will run on
+#user = 'user'          # the user gunicorn will run on
 
-daemon = True          # this is only to tell gunicorn to deamonize the server process
+#daemon = True          # this is only to tell gunicorn to deamonize the server process
 
-errorlog = '/var/log/gunicorn/error-mysite.log'    # error log
+#errorlog = '/var/log/gunicorn/error-mysite.log'    # error log
 
-accesslog = '/var/log/gunicorn/access-mysite.log'  # access log
+a#ccesslog = '/var/log/gunicorn/access-mysite.log'  # access log
 
-proc_name = 'gunicorn-mysite'            # the gunicorn process name
+proc_name = 'gunicorn-web'            # the gunicorn process name
 
 # run gunicorn with this file
-# gunicorn -c gunicorn_conf.py hello:app
+# gunicorn -c gunicorn_conf.py hello:appa
